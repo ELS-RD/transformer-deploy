6e21f7a8bcf839ffe861735f3461b6dc99c228e6
pommedeterresautee <pommedeterresautee@msn.com>
2021-11-02 21:11:37 +0100

    output

--- triton.py
+++ triton.py
@@ -13,6 +13,7 @@ triton_client = tritonclient.http.InferenceServerClient(url=url, verbose=False)
 time_buffer = list()
 input0 = tritonclient.http.InferInput('input_ids', (1, nb_tokens), 'INT64')
 input1 = tritonclient.http.InferInput('attention_mask', (1, nb_tokens), 'INT64')
+output = tritonclient.http.InferRequestedOutput('model_output', binary_data=False)
 
 # random input to avoid any caching
 input_ids = np.random.randint(100000, size=(1, nb_tokens), dtype=np.int64)
@@ -22,7 +23,7 @@ for _ in range(1000):
 
         input0.set_data_from_numpy(input_ids, binary_data=False)
         input1.set_data_from_numpy(attention_mask, binary_data=False)
-        output = tritonclient.http.InferRequestedOutput('model_output', binary_data=False)
+
         response = triton_client.infer(model_name, model_version=model_version, inputs=[input0, input1],
                                        outputs=[output])
         logits = response.as_numpy('model_output')

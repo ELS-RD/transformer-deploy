a4b33ae6a4d46d59a614d5846a21dedb27e2211d
pommedeterresautee <pommedeterresautee@msn.com>
2021-12-15 10:51:48 +0100

    add slow model tests

---
+++ tests/test_models.py
@@ -0,0 +1,59 @@
+#  Copyright 2021, Lefebvre Sarrut Services
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+import pytest
+
+from transformer_deploy.convert import main
+from transformer_deploy.utils.args import parse_args
+
+
+@pytest.mark.gpu
+def test_minilm():
+    commands = [
+        "-m",
+        "philschmid/MiniLM-L6-H384-uncased-sst2",
+        "--backend",
+        "tensorrt",
+        "onnx",
+        "-b",
+        "1",
+        "16",
+        "16",
+        "-s",
+        "8",
+        "8",
+        "8",
+    ]
+    args = parse_args(commands=commands)
+    main(commands=args)
+
+
+@pytest.mark.gpu
+def test_camembert():
+    commands = [
+        "-m",
+        "camembert-base",
+        "--backend",
+        "tensorrt",
+        "onnx",
+        "-b",
+        "1",
+        "16",
+        "16",
+        "-s",
+        "8",
+        "8",
+        "8",
+    ]
+    args = parse_args(commands=commands)
+    main(commands=args)

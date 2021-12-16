4cbc9261f7a66a7ec9c38f11648e42c93541eca8
pommedeterresautee <pommedeterresautee@msn.com>
2021-12-16 12:06:09 +0100

    fix test

--- src/transformer_deploy/convert.py
+++ src/transformer_deploy/convert.py
@@ -81,7 +81,7 @@ def main(commands: argparse.Namespace):
 
     torch.manual_seed(commands.seed)
     np.random.seed(commands.seed)
-    torch.set_num_interop_threads(commands.nb_threads)
+    torch.set_num_threads(commands.nb_threads)
 
     if isinstance(commands.auth_token, str) and commands.auth_token.lower() in ["true", "t"]:
         auth_token = True

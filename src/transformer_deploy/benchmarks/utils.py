05d5cab6cb38cd9bee59d4c98af9de9f27f9cfca
pommedeterresautee <pommedeterresautee@msn.com>
2022-01-02 17:57:40 +0100

    update License

--- src/transformer_deploy/benchmarks/utils.py
+++ src/transformer_deploy/benchmarks/utils.py
@@ -1,4 +1,4 @@
-#  Copyright 2021, Lefebvre Sarrut Services
+#  Copyright 2022, Lefebvre Dalloz Services
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.
--- src/transformer_deploy/convert.py
+++ src/transformer_deploy/convert.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-#  Copyright 2021, Lefebvre Sarrut Services
+#  Copyright 2022, Lefebvre Dalloz Services
 #
 #  Licensed under the Apache License, Version 2.0 (the "License");
 #  you may not use this file except in compliance with the License.

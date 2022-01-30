53c4c94d4a50898cc1b93b285473d075056fb41b
pommedeterresautee <pommedeterresautee@msn.com>
2022-01-30 23:55:49 +0100

    add cache / no cache XP

--- src/transformer_deploy/backends/ort_utils.py
+++ src/transformer_deploy/backends/ort_utils.py
@@ -170,8 +170,9 @@ def inference_onnx_binding(
     binding: IOBinding = model_onnx.io_binding()
     for input_onnx in model_onnx.get_inputs():
         tensor: torch.Tensor = inputs[input_onnx.name]
-        # int32 mandatory as input of bindings, int64 not supported
-        tensor = tensor.type(dtype=torch.int32).to(device)
+        if tensor.dtype in [torch.int64, torch.long]:
+            # int32 mandatory as input of bindings, int64 not supported
+            tensor = tensor.type(dtype=torch.int32).to(device)
         binding.bind_input(
             name=input_onnx.name,
             device_type=device,

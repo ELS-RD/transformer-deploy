7cb5bf23a1694b8d85123b2f093c5d48d909d728
pommedeterresautee <pommedeterresautee@msn.com>
2022-06-30 21:19:19 +0200

    feat: add inference function tests

--- tests/test_ort_utils.py
+++ tests/test_ort_utils.py
@@ -19,16 +19,97 @@ import onnx
 import pytest
 import torch
 from onnx import GraphProto, ModelProto, NodeProto, TensorProto, ValueInfoProto, helper
-from onnxruntime import OrtValue
+from onnxruntime import InferenceSession, OrtValue
 from pytest_benchmark.fixture import BenchmarkFixture
 
 from transformer_deploy.backends.onnx_utils import convert_bf16_to_fp32, convert_fp32_to_bf16
-from transformer_deploy.backends.ort_utils import get_io_to_node_mapping, ort_conversion_table, to_pytorch
+from transformer_deploy.backends.ort_utils import (
+    get_io_to_node_mapping,
+    inference_onnx_binding,
+    ort_conversion_table,
+    to_pytorch,
+)
+
+
+def get_simple_onnx(input_type: int) -> ModelProto:
+    """
+    Generate a very simple ONNX model
+    :return: Onnx model
+    """
+    # I/O
+    input_x: ValueInfoProto = helper.make_tensor_value_info("X", input_type, shape=["axis1", "axis2"])
+    out: ValueInfoProto = helper.make_tensor_value_info("OUT", input_type, shape=["axis1", "axis2"])
+
+    # Create a node (NodeProto)
+    add_1_node: NodeProto = helper.make_node(
+        op_type="Add",
+        inputs=["X", "Tensor"],
+        outputs=["add1"],
+        name="first_add",
+    )
+
+    add_2_node: NodeProto = helper.make_node(
+        op_type="Add",
+        inputs=["add1", "Tensor"],
+        outputs=["OUT"],
+        name="second_add",
+    )
+
+    init_tensor = helper.make_tensor("Tensor", dims=(1, 1), vals=[1], data_type=input_type)
+
+    graph_add: GraphProto = helper.make_graph(
+        nodes=[add_1_node, add_2_node],
+        name="add-model",
+        inputs=[input_x],
+        outputs=[out],
+        initializer=[init_tensor],
+    )
+
+    model_def: ModelProto = helper.make_model(
+        graph_add, producer_name="onnx-example", opset_imports=[helper.make_opsetid(onnx.defs.ONNX_DOMAIN, 13)]
+    )
+    onnx.checker.check_model(model_def)
+    return model_def
+
+
+def pytorch_inference(benchmark, ort_type: int, torch_type: torch.dtype, device: str):
+    model_float = get_simple_onnx(input_type=ort_type)
+    onnx_device = "CPUExecutionProvider" if device == "cpu" else "CUDAExecutionProvider"
+    ort_model = InferenceSession(model_float.SerializeToString(), providers=[onnx_device])
+    binding = ort_model.io_binding()
+    input = torch.rand((1, 1000)).type(torch_type).to(device)
+    expected = input + 2
+    results = benchmark(
+        inference_onnx_binding,
+        model_onnx=ort_model,
+        inputs={"X": input},
+        device="cpu",
+        binding=binding,
+        clone_tensor=False,
+    )
+    assert np.allclose(results["OUT"].cpu().numpy(), expected.cpu().numpy(), atol=1e-1)
+
+
+@pytest.mark.benchmark(group="pytorch_inference", disable_gc=True, warmup=True)
+def test_pytorch_inference_float32_cpu(benchmark):
+    pytorch_inference(benchmark=benchmark, ort_type=TensorProto.FLOAT, torch_type=torch.float32, device="cpu")
+
+
+@pytest.mark.benchmark(group="pytorch_inference", disable_gc=True, warmup=True)
+@pytest.mark.gpu
+def test_pytorch_inference_float32_cuda(benchmark):
+    pytorch_inference(benchmark=benchmark, ort_type=TensorProto.FLOAT, torch_type=torch.float32, device="cuda")
+
+
+@pytest.mark.benchmark(group="pytorch_inference", disable_gc=True, warmup=True)
+@pytest.mark.gpu
+def test_pytorch_inference_float16_cuda(benchmark):
+    pytorch_inference(benchmark=benchmark, ort_type=TensorProto.FLOAT16, torch_type=torch.float16, device="cuda")
 
 
 def get_onnx_if() -> ModelProto:
     """
-    Generate an Onnx model with an If node
+    Generate an ONNX model with an If node
     :return: Onnx model
     """
     # I/O
@@ -130,7 +211,9 @@ def get_onnx_if() -> ModelProto:
         initializer=[gather_dim_index, expected_val, squeeze_dim, init_tensor],
     )
 
-    model_def: ModelProto = helper.make_model(if_graph_def, producer_name="onnx-example")
+    model_def: ModelProto = helper.make_model(
+        if_graph_def, producer_name="onnx-example", opset_imports=[helper.make_opsetid(onnx.defs.ONNX_DOMAIN, 13)]
+    )
     onnx.checker.check_model(model_def)
     return model_def
 
@@ -162,8 +245,8 @@ def check_pytorch_conversion(data: np.ndarray, ortvalue: OrtValue, benchmark: Op
     assert np.allclose(tensor.cpu().numpy(), data)
 
 
-@pytest.mark.benchmark(group="pytorch_conversion", disable_gc=True, warmup=True)
-def test_conversion_cpu_float16(benchmark):
+@pytest.mark.benchmark(group="ort_pytorch_conversion", disable_gc=True, warmup=True)
+def test_ort_conversion_cpu_float16(benchmark):
     """
     float16 doesn't exist in ctypes, it will use an intermediate dtype during the conversion,
     making the conversion slower.
@@ -173,24 +256,24 @@ def test_conversion_cpu_float16(benchmark):
     check_pytorch_conversion(data=data, ortvalue=ortvalue, benchmark=benchmark)
 
 
-@pytest.mark.benchmark(group="pytorch_conversion", disable_gc=True, warmup=True)
-def test_conversion_cpu_float32(benchmark):
+@pytest.mark.benchmark(group="ort_pytorch_conversion", disable_gc=True, warmup=True)
+def test_ort_conversion_cpu_float32(benchmark):
     data = np.arange(1000, dtype=np.float32)
     ortvalue = OrtValue.ortvalue_from_numpy(data)
     check_pytorch_conversion(data=data, ortvalue=ortvalue, benchmark=benchmark)
 
 
-@pytest.mark.benchmark(group="pytorch_conversion", disable_gc=True, warmup=True)
+@pytest.mark.benchmark(group="ort_pytorch_conversion", disable_gc=True, warmup=True)
 @pytest.mark.gpu
-def test_conversion_gpu_float16(benchmark):
+def test_ort_conversion_gpu_float16(benchmark):
     data = np.arange(1000, dtype=np.float16)
     ortvalue = OrtValue.ortvalue_from_numpy(data, "cuda", 0)
     check_pytorch_conversion(data=data, ortvalue=ortvalue, benchmark=benchmark)
 
 
-@pytest.mark.benchmark(group="pytorch_conversion", disable_gc=True, warmup=True)
+@pytest.mark.benchmark(group="ort_pytorch_conversion", disable_gc=True, warmup=True)
 @pytest.mark.gpu
-def test_conversion_gpu_float32(benchmark):
+def test_ort_conversion_gpu_float32(benchmark):
     data = np.arange(1000, dtype=np.float32)
     ortvalue = OrtValue.ortvalue_from_numpy(data, "cuda", 0)
     check_pytorch_conversion(data=data, ortvalue=ortvalue, benchmark=benchmark)
